49af35448e88f1efc1de09e0bbde62fa388a5350
644727408@qq.com <644727408@qq.com>
2018-01-26 17:59:04 +0800

    Proxy

--- HURxSwiftNetWork/HURxSwiftNetWork/RxTableViewDelegateProxy.swift
+++ HURxSwiftNetWork/HURxSwiftNetWork/RxTableViewDelegateProxy.swift
@@ -10,9 +10,17 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class MyDelegateProxy: DelegateProxy<UITableView, UITableViewDelegate>, UITableViewDelegate, DelegateProxyType {
+class MyDelegateProxy: DelegateProxy<UITableView, UITableViewDelegate> ,UITableViewDelegate ,DelegateProxyType {
+    
+    public weak private(set) var tableView: UITableView?
+    
+    public init(tableView: ParentObject) {
+        self.tableView = tableView
+        super.init(parentObject: tableView, delegateProxy: MyDelegateProxy.self)
+    }
+    //  如果没有实现这个方法 下面两个方法也不会实现 就会报错！参考了 Rx 中的实现方式实现的
     static func registerKnownImplementations() {
-        
+        self.register { MyDelegateProxy(tableView: $0) }
     }
     
     static func setCurrentDelegate(_ delegate: UITableViewDelegate?, to object: UITableView) {
@@ -32,11 +40,18 @@ extension UITableView {
     var rxDelegate: MyDelegateProxy {
         return MyDelegateProxy.proxy(for: self)
     }
-    
+    // RxSwift 中对 Selector 绑定的方法已经修改 这里需要使用 methodInvoked 方法实现。
     var rxDidSelectRowAtIndexPath: Observable<(UITableView, IndexPath)> {
-        
-        let r = rxDelegate.observe(#selector(UITableViewDelegate.tableView(_:didSelectRowAt:)), changeHandler: <#(MyDelegateProxy, NSKeyValueObservedChange<Value>) -> Void#>)
+        return rxDelegate.methodInvoked(.didSelectRowAtIndexPath).map { a in
+            print(a)
+            return (a[0] as! UITableView, a[1] as! IndexPath)
+        }
+    }
+}
 
-        
+extension ViewController {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 视频中说 会先走 Proxy 中的方法，但是实践的时候我发现情况相反。先走原生代理方法，然后再执行 Proxy 中的订阅方法。
+        print("\(indexPath)+++====")
     }
 }
--- HURxSwiftNetWork/HURxSwiftNetWork/ViewController.swift
+++ HURxSwiftNetWork/HURxSwiftNetWork/ViewController.swift
@@ -12,12 +12,14 @@ import RxSwift
 import RxDataSources
 import MJRefresh
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate{
 
     var dataArray = Variable<[SectionModel<String, Model>]>([])
+//    var data = Variable<[Model]>([])
     var page = 0
     let bag = DisposeBag()
     let tableView = UITableView(frame: CGRect(x: 0, y: 20, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height - 20), style: .plain)
+    
     let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Model>>(configureCell:{ (source, tableview, index, model) -> UITableViewCell in
         let cell = tableview.dequeueReusableCell(withIdentifier: "Cell", for: index) as! TableViewCell
         cell.setupWithModel(model)
@@ -26,45 +28,39 @@ class ViewController: UIViewController {
     let setCell = {(i: Int,e : Model,c: TableViewCell) in
         c.setupWithModel(e)
     }
-    typealias SectionTableModel = SectionModel<String,Model>
-//    let dataSource = RxTableViewSectionedReloadDataSource<SectionTableModel>(configureCell:configureCell: )
     // 顶部刷新
     let header = MJRefreshNormalHeader()
     // 底部刷新
     let footer = MJRefreshAutoNormalFooter()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         view.addSubview(tableView)
-        setupRx()
-
         tableView.rowHeight = 60
         tableView.register(TableViewCell.self, forCellReuseIdentifier: "Cell")
+        // 注意: 委托代理一定要在绑定数据前执行，否则会造成崩溃！
+        tableView.rx.setDelegate(self).disposed(by: bag)
+        setupRx()
+
         upRefresh()
     }
 
     func setupRx () {
 
-//        dataArray.asObservable().bind(to: self.tableView.rx.items(cellIdentifier: "Cell", cellType: TableViewCell.self))(setCell).disposed(by: bag)
-//        let closer = tableView.rx.items(dataSource: dataSource)
-        dataSource.titleForHeaderInSection = { ds, ii in
-            return ds.sectionModels[ii].model
+        dataSource.titleForHeaderInSection = { ds, ind in
+            return ds.sectionModels[ind].items[0].title
         }
+        
         dataArray.asObservable().bind(to: self.tableView.rx.items(dataSource: dataSource)).disposed(by: bag)
         
+        tableView.rxDidSelectRowAtIndexPath.subscribe(onNext: { a, b in
+            print(b)
+        }).disposed(by: bag)
+
         header.setRefreshingTarget(self, refreshingAction: #selector(upRefresh))
         footer.setRefreshingTarget(self, refreshingAction: #selector(downRefresh))
         self.tableView.mj_header = header
         self.tableView.mj_footer = footer
-        
-        tableView.rx.itemSelected.subscribe(onNext: {
-            self.tableView.cellForRow(at: $0)?.isSelected = false
-            print("点击",$0)
-        }).disposed(by: bag)
-        
-        tableView.rx.modelSelected(Model.self).subscribe(onNext: { (model) in
-            print(model)
-        }).disposed(by: bag)
     }
 
     @objc func upRefresh() {
@@ -76,11 +72,6 @@ class ViewController: UIViewController {
             let sec = SectionModel(model: "header", items: $0)
             self.dataArray.value.append(sec)
             
-//            _ = $0.map { model in
-//                let sec = SectionModel(model: "header", items: [model])
-//                self.dataArray.value.append(sec)
-//                self.dataArray.value.append(model)
-//            }
         }).disposed(by: bag)
     }
 
@@ -92,10 +83,6 @@ class ViewController: UIViewController {
             let sec = SectionModel(model: "header", items: $0)
             self.dataArray.value.append(sec)
             
-//            self.dataArray.value.append(sec)
-//            _ = $0.map { model in
-//                self.dataArray.value.append(model)
-//            }
         }).disposed(by: bag)
     }
 }
